377d2015e401cd7d48ac309bcb510502c3b7d622
Theerapong Likhitsab <Theerapong5176@gmail.com>
2025-11-30 13:32:36 +0900

    allow health check endpoint

--- src/main/java/com/thee5176/ledger_command/security/WebSecurityConfig.java
+++ src/main/java/com/thee5176/ledger_command/security/WebSecurityConfig.java
@@ -25,7 +25,7 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/").permitAll()
+                .requestMatchers("/", "/health").permitAll()
                 .anyRequest().authenticated()
             );
 
