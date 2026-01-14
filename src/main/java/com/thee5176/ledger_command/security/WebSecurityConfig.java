32bf940e3e8b3adb833d4fbaa327f402e159b005
Theerapong Likhitsab <Theerapong5176@gmail.com>
2026-01-14 18:53:37 +0900

    setup audience validator

--- src/main/java/com/thee5176/ledger_command/security/WebSecurityConfig.java
+++ src/main/java/com/thee5176/ledger_command/security/WebSecurityConfig.java
@@ -7,6 +7,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.jwt.JwtValidators;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.servlet.util.matcher.PathPatternRequestMatcher;
@@ -22,6 +30,8 @@ public class WebSecurityConfig {
     private String issuer;
     @Value("${okta.oauth2.client-id}")
     private String clientId;
+    @Value("${okta.oauth2.audience}")
+    private String audience;
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
@@ -34,11 +44,27 @@ public class WebSecurityConfig {
     
         http.oauth2Login(withDefaults());
 
-        // audience validation is handled by Default OAuth2TokenValidators
         http.oauth2ResourceServer(oauth2 -> oauth2
-            .jwt(withDefaults()));
+            .jwt(jwt -> jwt
+                .decoder(jwtDecoder())
+            )
+
+            );
 
         return http.build();
+    }
+    
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        NimbusJwtDecoder jwtDecoder = JwtDecoders.fromIssuerLocation(issuer);
+        
+        OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator(audience);
+        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
+        OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator);
+
+        jwtDecoder.setJwtValidator(withAudience);
+        
+        return jwtDecoder;
     }
 
     private LogoutHandler oidclogoutHandler() {
